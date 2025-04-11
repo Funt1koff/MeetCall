60f810625f29440d3e5b986289e8d3ecfcfc6463
Sergey Funtikov <sfuntikoff@gmail.com>
2025-04-12 01:07:54 +0400

    add zipkin service

---
+++ zipkin-service/src/test/java/com/meetcall/zipkinservice/ZipkinServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meetcall.zipkinservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
